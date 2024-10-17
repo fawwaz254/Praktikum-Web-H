9ae38610a965330de8ca0fdda822507094418df3
fawwaz254 <118798933+fawwaz254@users.noreply.github.com>
2024-10-17 18:21:20 +0700

    script.js

--- MODUL 2/Demo/Demo 1/script.js
+++ MODUL 2/Demo/Demo 1/script.js
@@ -1,33 +1,30 @@
-// Menampilkan input ke display
 function insert(value) {
     document.getElementById('display').value += value;
 }
 
-// Fungsi untuk menghitung hasil
 function calculate() {
-    try {
-        let result = eval(document.getElementById('display').value);
-        document.getElementById('display').value = result;
-    } catch (error) {
-        document.getElementById('display').value = 'Error';
+    let expression = document.getElementById('display').value;
+    if (expression.includes('^')) {
+        let parts = expression.split('^');
+        let base = parseFloat(parts[0]);
+        let exponent = parseFloat(parts[1]);
+        document.getElementById('display').value = Math.pow(base, exponent);
+    } else {
+        try {
+            let result = eval(expression);
+            document.getElementById('display').value = result;
+        } catch (error) {
+            document.getElementById('display').value = 'Error';
+        }
     }
 }
 
-// Fungsi untuk clear display
 function clearDisplay() {
     document.getElementById('display').value = '';
 }
 
-// Fungsi untuk operasi pangkat
-function power() {
+function backspace() {
     let display = document.getElementById('display').value;
-    let values = display.split('^');
-    if (values.length === 2) {
-        let base = parseFloat(values[0]);
-        let exponent = parseFloat(values[1]);
-        let result = Math.pow(base, exponent);
-        document.getElementById('display').value = result;
-    } else {
-        document.getElementById('display').value += '^';
-    }
+    document.getElementById('display').value = display.slice(0, -1);
 }
+
